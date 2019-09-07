fbccf097499fa9b0da138be8127a375a096300bc
Andres Isaza <andres.isazaa1506@gmail.com>
2019-09-07 09:07:20 -0500

    tests correction

--- test/code-breaker-api-test.js
+++ test/code-breaker-api-test.js
@@ -1,89 +1,109 @@
-var supertest = require('supertest');
-var chai = require('chai');
-var should = require('should');
-var assert = require('assert');
-var app = require('../app/app');
+var supertest = require('supertest'),
+    chai = require('chai'),
+    should = require('should'),
+    assert = require('assert'),
+    app = require('../app/./app');
 
 var request = supertest(app);
 var expect = chai.expect;
 
-describe('GET /setsecret/:secret', function () {
-    it('should return code 200', (done) => {
+describe('GET /setsecret:number', function () {
+    it('should return code 200', function (done) {
         request.get('/setsecret/1234')
             .expect(200)
             .end(function (err, res) {
-                if (err) return done(err);
-                should.not.exist(err);
-                should.exist(res);
-                res.body.should.be.an.Object;
-                should.exist(res.body.message);
-                done(err);
+                if (err) return done(err)
+                done(err)
             });
     });
 
-    it('should return JSON Content-Type application/json', (done) => {
+    it('should return Content-Type application/json', function (done) {
         request.get('/setsecret/1234')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function (err, res) {
-                if (err) return done(err);
-                done(err);
+                if (err) return done(err)
+                done(err)
             });
     });
-})
 
-describe('GET /guess/:number', () => {
-    it('should return code 200', (done) => {
-        request.get('/guess/1234')
+    it('should return correct JSON Object ', function (done) {
+        request.get('/setsecret/1234')
             .expect(200)
+            .expect('Content-Type', /json/)
             .end(function (err, res) {
-                if (err) return done(err);
+                if (err) return done(err)
                 should.not.exist(err);
                 should.exist(res);
                 res.body.should.be.an.Object;
                 should.exist(res.body.message);
-
-                done(err);
+                done()
             });
     });
 
-    it('should return JSON Content-Type application/json', (done) => {
+    it('should return correct message text ', function (done) {
+        request.get('/setsecret/1234')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+                if (err) return done(err)
+                should.not.exist(err);
+                should.exist(res);
+                res.body.should.be.an.Object;
+                should.exist(res.body.message);
+                assert.equal('ok, let the game begins', res.body.message);
+                done()
+            });
+    });
+});
+
+describe('GET /guess/:number', function () {
+    it('should return code 200', function (done) {
+        request.get('/guess/1234')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err)
+                done(err)
+            });
+    });
+
+    it('should return Content-Type application/json', function (done) {
         request.get('/guess/1234')
             .expect(200)
             .expect('Content-Type', /json/)
             .end(function (err, res) {
-                if (err) return done(err);
-                done(err);
+                if (err) return done(err)
+                done(err)
             });
     });
 
-    it('should return a correct JSON object', (done) => {
+    it('should return correct JSON Object ', function (done) {
         request.get('/guess/1234')
             .expect(200)
+            .expect('Content-Type', /json/)
             .end(function (err, res) {
-                if (err) return done(err);
+                if (err) return done(err)
                 should.not.exist(err);
                 should.exist(res);
                 res.body.should.be.an.Object;
-                should.exist(res.body.message);
-
-                done(err);
+                should.exist(res.body.result);
+                done()
             });
     });
 
-    it('should return a correct answer', (done) => {
+    it('should return correct answer ', function (done) {
         request.get('/guess/1234')
             .expect(200)
+            .expect('Content-Type', /json/)
             .end(function (err, res) {
-                if (err) return done(err);
+                if (err) return done(err)
                 should.not.exist(err);
                 should.exist(res);
                 res.body.should.be.an.Object;
-                should.exist(res.body.message);
+                should.exist(res.body.result);
                 assert.equal('XXXX', res.body.result);
-
-                done(err);
+                done()
             });
     });
 
-})
+});
